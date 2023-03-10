ad9d16286b07f526083737258de61afb3b58cc39
Nico Zucca <nazucca7@gmail.com>
2023-03-10 14:58:14 -0700

    Initial work on control features for robot

---
+++ control/main.c
@@ -0,0 +1,106 @@
+// gcc -o test test.c -lm -lbcm2835
+
+#include <bcm2835.h>
+#include <stdio.h>
+#include <ncurses.h>
+#define MOTOR_L_R RPI_GPIO_P1_22
+#define MOTOR_L_F RPI_GPIO_P1_24
+#define MOTOR_R_F RPI_GPIO_P1_26
+#define MOTOR_R_R RPI_GPIO_P1_18
+
+ #include <unistd.h>
+ #include <termios.h>
+#include <unistd.h>
+
+void setup_terminal() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+char read_keyboard_input() {
+    char c;
+    read(STDIN_FILENO, &c, 1);
+    return c;
+}
+
+int main(int argc, char **argv)
+{
+    // If you call this, it will not actually access the GPIO
+    // Use for testing
+    // bcm2835_set_debug(1);
+ setup_terminal();
+    if (!bcm2835_init())
+      return 1;
+ 
+    // Set the pin to be an output
+    bcm2835_gpio_fsel(MOTOR_L_R, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(MOTOR_L_F, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(MOTOR_R_F, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(MOTOR_R_R, BCM2835_GPIO_FSEL_OUTP);
+    int c;
+    // Blink
+    while (1)
+    {
+            c = read_keyboard_input();
+            if (c == 'w') {
+                // Move the robot forward
+                printf("For\n");
+                bcm2835_gpio_write(MOTOR_L_F, HIGH);
+                bcm2835_gpio_write(MOTOR_L_R, LOW);
+                bcm2835_gpio_write(MOTOR_R_F, HIGH);
+                bcm2835_gpio_write(MOTOR_R_R, LOW);
+            } else if (c == 'a') {
+                // Turn the robot left
+                printf("L\n");
+                bcm2835_gpio_write(MOTOR_L_F, LOW);
+                bcm2835_gpio_write(MOTOR_L_R, HIGH);
+                bcm2835_gpio_write(MOTOR_R_F, HIGH);
+                bcm2835_gpio_write(MOTOR_R_R, LOW);
+            } else if (c == 's') {
+                // Move the robot backward
+                printf("Rev\n");
+                bcm2835_gpio_write(MOTOR_L_F, LOW);
+                bcm2835_gpio_write(MOTOR_L_R, HIGH);
+                bcm2835_gpio_write(MOTOR_R_F, LOW);
+                bcm2835_gpio_write(MOTOR_R_R, HIGH);
+            } else if (c == 'd') {
+                // Turn the robot right
+                printf("R\n");
+                bcm2835_gpio_write(MOTOR_L_F, HIGH);
+                bcm2835_gpio_write(MOTOR_L_R, LOW);
+                bcm2835_gpio_write(MOTOR_R_F, LOW);
+                bcm2835_gpio_write(MOTOR_R_R, HIGH);
+            } else if (c == ' ') {
+                // Quit the program
+                printf("STOP\n");
+                bcm2835_gpio_write(MOTOR_L_F, LOW);
+                bcm2835_gpio_write(MOTOR_L_R, LOW);
+                bcm2835_gpio_write(MOTOR_R_F, LOW);
+                bcm2835_gpio_write(MOTOR_R_R, LOW);
+            } else {
+                // Invalid input
+                printf("Invalid input: %c\n", c);
+            }
+
+        // Perform other tasks here
+        // bcm2835_gpio_write(MOTOR_L_R, LOW);
+        // bcm2835_gpio_write(MOTOR_L_F, LOW);
+        // bcm2835_gpio_write(MOTOR_R_F, LOW);
+        // bcm2835_gpio_write(MOTOR_R_R, LOW);
+        // // Turn it on
+        // bcm2835_gpio_write(PIN, HIGH);
+        
+        // // wait a bit
+        // bcm2835_delay(500);
+        
+        // // turn it off
+        // bcm2835_gpio_write(PIN, LOW);
+        
+        // // wait a bit
+        // bcm2835_delay(500);
+    }
+    bcm2835_close();
+    return 0;
+}
